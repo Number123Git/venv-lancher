5959a75036d4acf2aa9d655127df0f01a79f6597
Number_123 <yuuma8085@icloud.com>
2025-07-27 16:02:26 +0900

    comment: entryの内容を取得することを表すコメント追加

--- main.py
+++ main.py
@@ -34,14 +34,15 @@ root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 # 出力先フォルダの選択
 output_label = ttk.Label(root, text="環境構築するフォルダを選択", font=("Yu Gothic UI", 11))
 output_dir_entry = ttk.Entry(root, width=50, font=("Yu Gothic UI", 11))
-output_dir = output_dir_entry.get()
+output_dir = output_dir_entry.get()  # 出力フォルダのパスを取得
+# 参照ボタンの作成
 output_button = ttk.Button(root, text="参照", command=dirdialog_clicked)
 
 
 # 環境名の入力
 env_name_label = ttk.Label(root, text="環境名を入力", font=("Yu Gothic UI", 11))
 env_name_entry = ttk.Entry(root, width=50, font=("Yu Gothic UI", 11))
-env_name = env_name_entry.get()
+env_name = env_name_entry.get()  # 環境名の取得
 
 
 # インストールするパッケージの入力
