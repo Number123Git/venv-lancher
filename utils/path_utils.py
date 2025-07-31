c0ce295f89af4f081fb1623e7f53fa87133528a8
Number_123 <yuuma8085@icloud.com>
2025-08-01 06:39:28 +0900

    feat: ディレクトリの存在を確かめる機能を追加

--- utils/path_utils.py
+++ utils/path_utils.py
@@ -1,2 +1,8 @@
 # ライブラリのインポート
 import os
+
+def path_check(output_dir):
+    if os.path.isdir(output_dir):
+        return True  # 出力先フォルダが存在する場合はTrueを返す
+    else:
+        return False  # 出力先フォルダが存在しない場合はFalseを返す
