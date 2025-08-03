9ce34b8a3bec2f8f7e32ee6890d3d3f144fedab7
Number_123 <yuuma8085@icloud.com>
2025-08-03 16:00:43 +0900

    feat: 出力先フォルダが存在しない場合は通知を表示する機能を追加

--- utils/path_utils.py
+++ utils/path_utils.py
@@ -6,4 +6,5 @@ def path_check(output_dir):
     if os.path.isdir(output_dir):
         return True  # 出力先フォルダが存在する場合はTrueを返す
     else:
+        toast("エラー", "入力された出力先フォルダが存在しません。")  # 出力先フォルダが存在しない場合は通知を表示
         return False  # 出力先フォルダが存在しない場合はFalseを返す
